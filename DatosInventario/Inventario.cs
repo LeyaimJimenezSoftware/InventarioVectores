b729dbfa8685869c9ad2eff1fd5114fd11d3eca0
LeyaimJimenezSoftware <ljimenez6@ucol.mx>
2017-04-11 18:45:23 -0500

    Inventario

---
+++ DatosInventario/Inventario.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DatosInventario
+{
+    class Inventario
+    {
+        Producto[] productos = new Producto[15];
+
+        Producto productosAgragar;// = new Producto("","","","");
+        int cont;
+        int pos;
+        Producto auxiliar;
+
+        private int c = 0;
+
+        string buscar = "";
+
+        public void ingressarProducto(Producto p)
+        {
+            bool repite = false;
+      
+            for (int i = 0; i < c; i++)
+            {
+                if (productos[i].codigo == p.codigo)
+                {
+
+
+                    repite = true;
+                    if(repite == true)
+                    {
+                        i = c-1;
+                       
+                    }
+                }
+
+            }
+            if (cont != 15 && repite == false)
+            {
+                productos[c] = p;
+                c++;
+            }
+            
+           
+        }
+        public string contador()
+        {
+            return c .ToString();
+        }
+        public void eliminarProducto(string codigo)
+        {
+        
+
+          
+            for (int i = 0; i < c; i++)
+            {
+                if (productos[i].codigo == codigo)
+                {
+                    for (int x = i; x < c; x++)
+                    {
+                        productos[x] = productos[x + 1];
+                    }
+                    productos[c] = null;
+                    c--;
+                   
+                }
+            }
+           
+        }
+
+        public Producto  buscarProducto(string codigo)
+        {
+            Producto buscado = null;
+            bool encontrado = false;
+            for (int i = 0; i < c; i++)
+            {
+                if (productos[i].codigo == codigo)
+                {
+                    encontrado = true;
+                    //buscar = "Codigo:" + productos[i].codigo + " Nombre:" + productos[i].nombre + " Precio:" + productos[i].precio + " Cantidad:" + productos[i].cantidad;
+                    buscado = productos[i];
+                    if(encontrado == true)
+                    {
+                        i = c - 1;
+                    }
+
+                }
+
+            
+
+            }
+            return buscado;
+
+        }
+
+        public void insertarProducto(Producto p , int pos)
+        {
+            if (pos < c)
+            {
+                c++;
+                if (productos[pos].codigo != null)
+                {
+                    for (int i = pos-1; i < c; i++)
+                    {
+                        auxiliar = productos[i];
+                        productos[i] = p;
+                        p = auxiliar;
+                       
+                    }
+                  
+
+                }
+                
+            }
+            else
+            {
+                productos[c] = p;
+                c++;
+
+            }
+
+        }
+
+        public override string ToString()
+        {
+            string str = " ";
+            //foreach(Producto p in productos)
+            //{
+            //    str = p.ToString();
+
+            //}
+
+            for (int i = 0; i < c; i++)
+            {
+                str += "\n " +(i+1)+":"+productos[i].ToString()+"\n "; 
+            }
+            return str;
+        }
+
+    }
+
+    }
+
---
+++ DatosInventario/Producto.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatosInventario
+{
+    class Producto
+    {
+        private string _codigo, _nombre, _precio, _cantidad;
+
+        public Producto(string codigo, string nombre, string precio, string cantidad)
+        {
+            this.codigo = codigo;
+            this.nombre = nombre;
+            this.precio = precio;
+            this.cantidad = cantidad;
+        }
+
+     
+         public string codigo
+        {
+            get
+            {
+                return _codigo;
+            }
+            set
+            {
+                _codigo = value;
+            }
+        }
+        public string nombre
+        {
+            get
+            {
+                return _nombre;
+            }
+            set
+            {
+                _nombre = value;
+            }
+        }
+        public string precio
+        {
+            get
+            {
+                return _precio;
+            }
+            set
+            {
+                _precio = value;
+            }
+        }
+        public string cantidad
+        {
+            get
+            {
+                return _cantidad;
+            }
+            set
+            {
+                _cantidad = value;
+            }
+        }
+        public override string ToString()
+        {
+            string listaProductos = null;
+            listaProductos = "Codigo: " + _codigo +" Nombre: " + _nombre + " Precio: " + _precio + " Cantidad: " + _cantidad + "\r\n" + "\r\n";
+           
+            return listaProductos;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
